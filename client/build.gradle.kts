aef4d5148426ee855785f2151d1b582617bf9050
York Wei <york.wei@icloud.com>
2023-02-06 21:31:49 -0500

    fix client run

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("multiplatform")
+    kotlin("jvm")
     id("org.jetbrains.compose")
 }
 
@@ -16,37 +16,15 @@ repositories {
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
 }
 
-kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "11"
-        }
-        withJava()
-    }
-    sourceSets {
-        val jvmMain by getting {
-            dependencies {
-                implementation(compose.desktop.currentOs)
-                implementation(project(":shared"))
-            }
-        }
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-                implementation(project(":shared"))
-            }
-        }
-    }
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(project(":shared"))
+    testImplementation(kotlin("test"))
 }
 
 compose.desktop {
     application {
         mainClass = "MainKt"
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "client"
-            packageVersion = "1.0.0"
-        }
     }
 }
 
