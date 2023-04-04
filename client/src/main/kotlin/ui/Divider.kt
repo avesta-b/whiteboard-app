4cd7fe910129e86a213ab163bbbbb69f0191566b
Mikail Rahman <am5rahman@uwaterloo.ca>
2023-04-04 15:18:42 -0400

    Chat cleanup (#20)

--- client/src/main/kotlin/ui/Divider.kt
+++ client/src/main/kotlin/ui/Divider.kt
@@ -1,9 +1,7 @@
 package cs346.whiteboard.client.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,6 +18,20 @@ fun VerticalDivider(
         modifier
             .fillMaxHeight()
             .width(thickness)
+            .background(color = color)
+    )
+}
+
+@Composable
+fun HorizontalDivider(
+    modifier: Modifier = Modifier,
+    color: Color,
+    thickness: Dp = 1.dp
+) {
+    Box(
+        modifier
+            .fillMaxWidth()
+            .height(thickness)
             .background(color = color)
     )
 }
